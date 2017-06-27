9edb7f39a0d7b9b08a27e1390ce19c911be3df27
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-06-27 14:20:02 +0800

    Improve AddressBook Javadoc

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -31,7 +31,7 @@ import java.util.Set;
  */
 
 /**
- * This class is used to maintain a list of person data which are saved
+ * Maintains a list of person data which are saved
  * in a text file.
  **/
 public class AddressBook {
